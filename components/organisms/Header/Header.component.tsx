daae44927062313399712f691329d3cadda55334
Murat Umutlu <murat@muum.dev>
2024-08-12 14:21:28 +0300

    added info about myself

--- components/organisms/Header/Header.component.tsx
+++ components/organisms/Header/Header.component.tsx
@@ -1,35 +1,49 @@
 /* eslint-disable import/order */
-import { Logo, ThemeSwitcher } from '@/components';
-import { ActionIcon, Container, Group } from '@mantine/core';
+import { Logo, ThemeSwitcher } from "@/components";
+import { ActionIcon, Container, Group } from "@mantine/core";
 
-import { IconQuestionMark } from '@tabler/icons-react';
-import classes from './Header.module.css';
+import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
+import classes from "./Header.module.css";
 
-import router from 'next/router';
+import router from "next/router";
 
 export default function Header() {
-  const handleQuestionMarkClick = () => {
-    router.push('/about');
-  };
+	const handleLinkedinClick = () => {
+		router.push("https://linkedin.com/in/murat-umutlu");
+	};
 
-  return (
-    <header className={classes.header}>
-      <Container size="xl" className={classes.inner}>
-        <Logo brand="🔍 Muum Codes" />
-        <Group>
-          <ThemeSwitcher />
-          <ActionIcon
-            aria-label="Search"
-            radius="xl"
-            size="lg"
-            variant="outline"
-            color="gray"
-            onClick={handleQuestionMarkClick}
-          >
-            <IconQuestionMark />
-          </ActionIcon>
-        </Group>
-      </Container>
-    </header>
-  );
+	const handleGithubClick = () => {
+		router.push("https://github.com/muratumutlu/muum-codes");
+	};
+
+	return (
+		<header className={classes.header}>
+			<Container size="xl" className={classes.inner}>
+				<Logo brand="🔍 Muum Codes" />
+				<Group>
+					<ThemeSwitcher />
+					<ActionIcon
+						aria-label="Who am I?"
+						radius="xl"
+						size="lg"
+						variant="transparent"
+						color="gray"
+						onClick={handleLinkedinClick}
+					>
+						<IconBrandLinkedin />
+					</ActionIcon>
+					<ActionIcon
+						aria-label="Github Repository"
+						radius="xl"
+						size="lg"
+						variant="transparent"
+						color="gray"
+						onClick={handleGithubClick}
+					>
+						<IconBrandGithub />
+					</ActionIcon>
+				</Group>
+			</Container>
+		</header>
+	);
 }
